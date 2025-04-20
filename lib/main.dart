942f2cdb05d2f8a0a6843881603d14a6f12e9ed6
SANDIPpandey1 <102663858+SANDIPpandey1@users.noreply.github.com>
2025-04-20 21:14:27 +0545

    Completed time 1:15:03

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_one/home_page.dart';
+import 'package:flutter_one/pages/home_page.dart';
+import 'package:flutter_one/pages/login_page.dart';
 
 void main() {
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
-  const myApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      // theme: ThemeData(primarySwatch: Colors.pink),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  var days = 20;
+  var name = "Sandip";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("My First App"), centerTitle: true),
+      appBar: AppBar(
+        title: Text("My First App"),
+        backgroundColor: Colors.pink,
+        centerTitle: true,
+      ),
       body: Center(
         child: Container(
           child: Text(
-            "My days = 30"
-            "\nMy Name is Sandip",
+            "My days = $days"
+            "\nMy Name is $name",
           ),
         ),
       ),
